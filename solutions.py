293172bfa5dd63899096fd9f0642d99ba716b7a0
@ProfNandaa <profnandaa@gmail.com>
2015-07-07 10:32:50 +0100

    Challenges

---
+++ solutions.py
@@ -0,0 +1,22 @@
+def max_int(a):
+	'''
+	The function returns the largest integer
+	in a given list a
+	'''
+	pass
+
+
+def half_reverse(s):
+	'''
+	Given a string s, the function reverses the second
+	half of the string
+	'''
+	pass
+
+def list_power(a):
+	'''
+	The function raises every integer in the list to the 
+	power of it's index. e.g. a = [2,4,3] returns [0,1,8]
+	'''
+	pass
+
